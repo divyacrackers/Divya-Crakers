f7cc028ef075ad7aabc0d52889bc6e6211348d02
sarayupotnuru29-svg <sarayupotnuru29@gmail.com>
2026-04-15 12:50:03 +0530

    updated

--- src/app/data/products.ts
+++ src/app/data/products.ts
@@ -27,6 +27,16 @@ import ShowTime from '../../assets/show-time.png';
 import SoundCrackers from '../../assets/sound-crackers.png';
 import Sparklers from '../../assets/sparklers.png';
 import Spinners from '../../assets/spinners.png';
+import BigBang from '../../assets/big-bang-racing-colors.png';
+import KaraokeNight from '../../assets/karaoke-night.png';
+import Kidsfountain from '../../assets/kids-fountain.png';
+import tencmsparkler from '../../assets/10cm-sparkler.png';
+import twelevcmsparkler from '../../assets/12cm-sparkler.png';
+import fifteencmsparkler from '../../assets/15cm-sparkler.png';
+import thirtycmsparkler from '../../assets/30cm-sparkler.png';
+import sevencmsparkler from '../../assets/7cm-sparkler.png';
+import coloursparkler from '../../assets/colour-sparkler.png';
+import umberallasparkler from '../../assets/umberalla-sparkler.png';
 
 export interface Product {
   id: string;
@@ -78,4 +88,20 @@ export const products: Product[] = [
   { id: '26', name: 'Sound Crackers', price: 550, category: 'Sound', image: SoundCrackers },
   { id: '27', name: 'Sparklers', price: 100, category: 'Sparklers', image: Sparklers },
   { id: '28', name: 'Spinners', price: 150, category: 'Spinners', image: Spinners },
+
+  // Row 8
+  { id: '29', name: 'Big Bang Racing Colours', price: 3500, category: 'Fountains', image: BigBang },
+  { id: '30', name: 'Karaoke Night', price: 3500, category: 'Fountains', image: KaraokeNight},
+  { id: '31', name: 'Kids Fountain', price: 3500, category: 'Kids', image: Kidsfountain},
+  { id: '32', name: '10cm Sparkler', price: 3500, category: 'Sparklers', image: tencmsparkler},  
+  
+  //Row 9
+  { id: '33', name: '12cm Sparkler', price: 3500, category: 'Sparklers', image: twelevcmsparkler},
+  { id: '34', name: '15cm Sparkler', price: 3500, category: 'Sparklers', image: fifteencmsparkler},
+  { id: '35', name: '30cm Sparkler', price: 3500, category: 'Sparklers', image: thirtycmsparkler}, 
+  { id: '36', name: '7cm Sparkler', price: 3500, category: 'Sparklers', image: sevencmsparkler},    
+  
+  //Row 10
+  { id: '37', name: 'Colour Sparkler', price: 3500, category: 'Sparklers', image: coloursparkler},   
+  { id: '38', name: 'Umberalla Sparkler', price: 3500, category: 'Sparklers', image: umberallasparkler},     
 ];
